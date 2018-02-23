a791fa15c00b09e7f1a4431bcdac1ba8fa76f672
Oscar Diaz <oscar.dh@unitec.edu>
2018-02-22 22:57:04 -0600

    Styling

--- ExamenUX/app/controllers/categories_controller.rb
+++ ExamenUX/app/controllers/categories_controller.rb
@@ -1,2 +1,51 @@
 class CategoriesController < ApplicationController
+	before_action :set_category, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@categories = Category.all
+	end
+
+	def new
+		@category = Category.new
+	end
+
+	def create
+		@category = Category.new(category_params)
+
+		if @category.save
+			redirect_to categories_path
+		else
+			render 'new'
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @category.update(category_params)
+			redirect_to @category
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+    	@category = Category.find(params[:id])
+		@category.destroy
+
+		redirect_to categories_path
+	end
+
+	private
+	def set_category
+		@category = Category.find(params[:id])
+	end
+
+	def category_params
+		params.require(:category).permit(:name)
+	end
 end
--- ExamenUX/app/controllers/todos_controller.rb
+++ ExamenUX/app/controllers/todos_controller.rb
@@ -1,2 +1,54 @@
 class TodosController < ApplicationController
+	before_action :set_todo, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@todos = Todo.all
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def new
+		@todo = Todo.new
+		@categories = Category.all
+	end
+
+	def create
+		@todo = Todo.new(todo_params)
+
+		if @todo.save
+			redirect_to todos_path
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		
+		if @todo.update(todo_params)
+			redirect_to @todo
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+    	@todo = Todo.find(params[:id])
+		@todo.destroy
+		redirect_to todos_path
+	end
+
+
+	private
+	def set_todo
+		@todo = Todo.find(params[:id])
+	end
+
+	def todo_params
+		params.require(:todo).permit(:category_id, :description, :level)
+	end
 end
+
